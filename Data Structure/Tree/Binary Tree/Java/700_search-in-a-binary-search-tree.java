f7a12b9145cfb1607d623449317125b620ccf9f3
David Jiang <jiangguanlin2003@outlook.com>
2022-07-25 18:03:54 +0800

    Create 700_search-in-a-binary-search-tree.java

---
+++ Data Structure/Tree/Binary Tree/Java/700_search-in-a-binary-search-tree.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) {
+            return null;
+        }
+        
+        while (root != null && root.val != val) {
+            if (val < root.val) {
+                root = root.left;
+            } else {
+                root = root.right;
+            }
+        }
+        return root;
+    }
+}
