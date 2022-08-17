a89aea29b085aaa2c800afe43c71741cd1649a7b
David Jiang <jiangguanlin2003@outlook.com>
2022-08-17 18:16:22 +0800

    Create 653_two-sum-iv-input-is-a-bst.java

---
+++ Data Structure/Tree/Binary Tree/Binary Search Tree/Java/653_two-sum-iv-input-is-a-bst.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    ArrayList<Integer> list = new ArrayList<>();
+    
+    public boolean findTarget(TreeNode root, int k) {
+        inorder(root);
+        for (int i = 0; i < list.size() - 1; i++) {
+            for (int j = i + 1; j < list.size(); j++) {
+                if ((list.get(i) + list.get(j)) == k) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    public void inorder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left);
+        list.add(root.val);
+        inorder(root.right);
+    }
+}
