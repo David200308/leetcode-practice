4463812a12be688d098252165a336def25ecb701
David Jiang <jiangguanlin2003@outlook.com>
2022-08-30 19:30:55 +0800

    Add

---
+++ Data Structure/Tree/Binary Tree/Binary Search Tree/Java/1305_all-elements-in-two-binary-search-trees.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<Integer> list = new ArrayList();
+    public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+        inorder(root1);
+        inorder(root2);
+
+        Collections.sort(list);
+        return list;
+        
+    }
+    
+    public void inorder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left);
+        list.add(root.val);
+        inorder(root.right);
+    }
+}
