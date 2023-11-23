403330b2748f03a1b8e6fe487e34fb35101a5eb6
David Jiang <jiangguanlin2003@outlook.com>
2023-11-23 11:55:04 +0800

    Create 258_add-digits.java

---
+++ Algorithms/Math/Java/258_add-digits.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int addDigits(int num) {
+        if (num < 10) {
+            return num;
+        }
+
+        int sum = 0;
+        while (num > 0) {
+            sum = sum + num % 10;
+            num = num / 10;
+        }
+        
+        return addDigits(sum);
+    }
+}
