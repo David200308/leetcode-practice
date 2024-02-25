d276737faae1cea175b68efeed2df12e5ad71c3f
David Jiang <jiangguanlin2003@outlook.com>
2024-02-25 09:51:54 +0800

    Create 1071_greatest-common-divisor-of-strings.java

---
+++ Algorithms/Math/Java/1071_greatest-common-divisor-of-strings.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        if (!(str1 + str2).equals(str2 + str1)) {
+            return "";
+        }
+        int length = gcd(str1.length(), str2.length());
+        
+        return str1.substring(0, length);
+    }
+
+    public int gcd(int x, int y) {
+        if (y == 0) {
+            return x;
+        }
+        return gcd(y, x % y);
+    }
+}
